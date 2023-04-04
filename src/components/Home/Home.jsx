20b93262822a7b50fa319fd09492b3b13f6d8cb6
jihad-hossain1 <jihadkhan934@gmail.com>
2023-04-04 17:33:59 +0600

    loading spanner added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigation } from 'react-router-dom';
 import Header from '../Header/Header';
 
 const Home = () => {
+    const navigation = useNavigation();
     return (
         <div>
             <Header></Header>
-            <h2>This are the outlet page . showing without any reload link up any thing  ........</h2>
+            <div>{navigation.state === 'loading' && 'loading....'}</div>
             <hr className='my-3 mx-3 '/>
             <Outlet></Outlet>
         </div>
